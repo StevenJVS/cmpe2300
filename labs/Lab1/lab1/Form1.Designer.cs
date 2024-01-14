03ea5cfbd84fb143bc9ab8d40038fd9db564baf9
Steven Santiago <122571554+StevenJVS@users.noreply.github.com>
2024-01-13 19:23:14 -0700

    something

--- labs/Lab1/lab1/Form1.Designer.cs
+++ labs/Lab1/lab1/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UI_Load_Btn = new System.Windows.Forms.Button();
+            this.UI_Solve_Btn = new System.Windows.Forms.Button();
+            this.UI_Display = new System.Windows.Forms.PictureBox();
+            this.UI_Message_txtbx = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_Display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_Load_Btn
+            // 
+            this.UI_Load_Btn.Location = new System.Drawing.Point(12, 380);
+            this.UI_Load_Btn.Name = "UI_Load_Btn";
+            this.UI_Load_Btn.Size = new System.Drawing.Size(75, 23);
+            this.UI_Load_Btn.TabIndex = 0;
+            this.UI_Load_Btn.Text = "Load Maze";
+            this.UI_Load_Btn.UseVisualStyleBackColor = true;
+            this.UI_Load_Btn.Click += new System.EventHandler(this.UI_Load_Btn_Click);
+            // 
+            // UI_Solve_Btn
+            // 
+            this.UI_Solve_Btn.Enabled = false;
+            this.UI_Solve_Btn.Location = new System.Drawing.Point(406, 380);
+            this.UI_Solve_Btn.Name = "UI_Solve_Btn";
+            this.UI_Solve_Btn.Size = new System.Drawing.Size(75, 23);
+            this.UI_Solve_Btn.TabIndex = 1;
+            this.UI_Solve_Btn.Text = "Solve Maze";
+            this.UI_Solve_Btn.UseVisualStyleBackColor = true;
+            this.UI_Solve_Btn.Click += new System.EventHandler(this.UI_Solve_Btn_Click);
+            // 
+            // UI_Display
+            // 
+            this.UI_Display.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.UI_Display.Location = new System.Drawing.Point(78, 12);
+            this.UI_Display.Name = "UI_Display";
+            this.UI_Display.Size = new System.Drawing.Size(348, 271);
+            this.UI_Display.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.UI_Display.TabIndex = 2;
+            this.UI_Display.TabStop = false;
+            // 
+            // UI_Message_txtbx
+            // 
+            this.UI_Message_txtbx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UI_Message_txtbx.Location = new System.Drawing.Point(35, 295);
+            this.UI_Message_txtbx.Name = "UI_Message_txtbx";
+            this.UI_Message_txtbx.Size = new System.Drawing.Size(422, 75);
+            this.UI_Message_txtbx.TabIndex = 3;
+            this.UI_Message_txtbx.Text = "Please load a maze";
+            this.UI_Message_txtbx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(493, 414);
+            this.Controls.Add(this.UI_Message_txtbx);
+            this.Controls.Add(this.UI_Display);
+            this.Controls.Add(this.UI_Solve_Btn);
+            this.Controls.Add(this.UI_Load_Btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_Display)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button UI_Load_Btn;
+        private System.Windows.Forms.Button UI_Solve_Btn;
+        private System.Windows.Forms.PictureBox UI_Display;
+        private System.Windows.Forms.Label UI_Message_txtbx;
     }
 }
 
--- labs/Lab1/lab1/Form1.cs
+++ labs/Lab1/lab1/Form1.cs
@@ -7,16 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDIDrawer;
 
 namespace lab1
 {
     public partial class Form1 : Form
     {
+        Bitmap maze;
+        public enum State {open, wall, visited};
+        State[,] blockstate;
+        Point start;
+        Point end;
         public Form1()
         {
             InitializeComponent();
             //start
             //newedit2
+        }
+
+        private void UI_Load_Btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                maze = new Bitmap(ofd.FileName);
+                if(maze.Width > 200 || maze.Height > 100)
+                {
+                    UI_Message_txtbx.Text = "Maze size is too big!";
+                }
+                else
+                {
+                    UI_Display.Image = Image.FromFile(ofd.FileName);
+                    UI_Message_txtbx.Text = $"Loaded: {maze.Width}x{maze.Height}";
+                    UI_Solve_Btn.Enabled = true;
+                    blockstate = new State[maze.Width, maze.Height];
+
+                    //create canvas
+                    CDrawer canvas = new CDrawer(maze.Width*10, maze.Height*10);
+                    canvas.Scale = 10;
+
+                    for (int i = 0; i < maze.Width; i++)
+                    {
+                        for(int j = 0; j < maze.Height; j++)
+                        {
+                            Color pixel = maze.GetPixel(i, j);
+                            if (pixel.R == 255 && pixel.G == 255 && pixel.B == 255)
+                            {
+                                blockstate[i, j] = State.open;
+                                canvas.SetBBScaledPixel(i, j, pixel);
+                            }
+                            else if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                            {
+                                blockstate[i, j] = State.wall;
+                                canvas.SetBBScaledPixel(i, j, pixel);
+                            }
+                            else if (pixel.R == 0 && pixel.G == 255 && pixel.B == 0)
+                            {
+                                canvas.SetBBScaledPixel(i, j, pixel);
+                                start.X = i; start.Y = j;
+                            }
+                            else if (pixel.R == 255 && pixel.G == 0 && pixel.B == 0)
+                            {
+                                canvas.SetBBScaledPixel(i, j, pixel);
+                                end.X = i; end.Y = j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UI_Solve_Btn_Click(object sender, EventArgs e)
+        {
+            if (blockstate != null) { }
+            Solve(start.X, start.Y);
+        }
+        private void Solve(int x, int y)
+        {
+            if()
         }
     }
 }
