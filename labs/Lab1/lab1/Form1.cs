2ec82cc4b96b89660b8b76eae12578859d84f469
Steven Santiago <122571554+StevenJVS@users.noreply.github.com>
2024-01-16 17:39:11 -0700

    update

--- labs/Lab1/lab1/Form1.cs
+++ labs/Lab1/lab1/Form1.cs
@@ -1,14 +1,9 @@
-﻿using System;
+﻿using GDIDrawer;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using GDIDrawer;
 
 namespace lab1
 {
@@ -16,10 +11,12 @@ namespace lab1
     {
         Bitmap maze;
         CDrawer canvas;
-        public enum State {open, wall, visited};
+        public enum State { open, wall, visited };
         State[,] blockstate;
         Point start;
         Point end;
+
+        Thread solve;
         int steps = 0;
 
         public Form1()
@@ -32,28 +29,32 @@ namespace lab1
         private void UI_Load_Btn_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            
-            if(ofd.ShowDialog() == DialogResult.OK)
+
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
                 maze = new Bitmap(ofd.FileName);
-                if(maze.Width > 200 || maze.Height > 100)
+                if (maze.Width > 200 || maze.Height > 100)
                 {
                     UI_Message_txtbx.Text = "Maze size is too big!";
                 }
                 else
                 {
+                    if (canvas != null)
+                    {
+                        canvas.Close();
+                    }
                     UI_Display.Image = Image.FromFile(ofd.FileName);
                     UI_Message_txtbx.Text = $"Loaded: {maze.Width}x{maze.Height}";
                     UI_Solve_Btn.Enabled = true;
                     blockstate = new State[maze.Width, maze.Height];
 
                     //create canvas
-                    canvas = new CDrawer(maze.Width*10, maze.Height*10);
+                    canvas = new CDrawer(maze.Width * 10, maze.Height * 10);
                     canvas.Scale = 10;
 
                     for (int i = 0; i < maze.Width; i++)
                     {
-                        for(int j = 0; j < maze.Height; j++)
+                        for (int j = 0; j < maze.Height; j++)
                         {
                             Color pixel = maze.GetPixel(i, j);
                             if (pixel.R == 255 && pixel.G == 255 && pixel.B == 255)
@@ -84,13 +85,18 @@ namespace lab1
 
         private void UI_Solve_Btn_Click(object sender, EventArgs e)
         {
-            if (blockstate != null) {
-                Color color = Color.Green;
-                Thread solve = new Thread(Solve);
+            if (blockstate != null)
+            {
+                solve = new Thread(CallSolve);
+                if(solve.ThreadState != ThreadState.Running)
                 solve.Start();
             }
         }
-        public int Solve()
+        public void CallSolve()
+        {
+            Solve(start);
+        }
+        public int Solve(Point curr)
         {
             if (curr.X == end.X && curr.Y == end.Y)
                 return 1;
@@ -99,26 +105,43 @@ namespace lab1
                 return 0;
             }
             canvas.SetBBScaledPixel(curr.X, curr.Y, Color.Purple);//paint canvas
+            Thread.Sleep(20);
+            blockstate[curr.X, curr.Y] = State.visited;
 
-            CurrPoint pointright = thispoint;
-            pointright.point.X = pointright.point.X + 1;
-            pointright.color = color;
+            
+            List <Point> paths = path(curr);
 
-            CurrPoint pointleft = thispoint;
-            pointleft.point.X = pointleft.point.X - 1;
-            pointleft.color = color;
+            foreach(Point p in paths)
+            {
+                Solve(p);
+            }
 
-            CurrPoint pointup = thispoint;
-            pointup.point.Y = pointup.point.Y - 1;
-            pointup.color = color;
+            if(paths.Count== 0)
+            { return 1; }
 
-            CurrPoint pointdown = thispoint;
-            pointdown.point.Y = pointdown.point.Y + 1;
-            pointdown.color = color;
-                Solve();
-                Solve();
-                Solve();
-                Solve();
+            return 0;
+        }
+        public List<Point> path(Point nextpoint)
+        {
+            List<Point> path = new List<Point>();
+
+            if (blockstate[nextpoint.X + 1, nextpoint.Y] == State.open && nextpoint.X > maze.Width)
+            {
+                path.Add(nextpoint);
+            }
+            if (blockstate[nextpoint.X - 1, nextpoint.Y] == State.open && nextpoint.X < 0)
+            {
+                path.Add(nextpoint);
+            }
+            if (blockstate[nextpoint.X, nextpoint.Y + 1] == State.open && nextpoint.Y > maze.Height)
+            {
+                path.Add(nextpoint);
+            }
+            if (blockstate[nextpoint.X, nextpoint.Y - 1] == State.open && nextpoint.Y < 0)
+            {
+                path.Add(nextpoint);
+            }
+            return path;
         }
     }
 }
